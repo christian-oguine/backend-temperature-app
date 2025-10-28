f7a3a69c02edac6d85de3693643c065301a95cce
Oguine Christian <christian.oguine@student.howest.be>
2025-10-28 13:01:46 +0100

    Weather endpoint to return JSON response with dummy city data

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');          // ✅ import cors
+const cors = require('cors');          
 
 const app = express();
-const port = process.env.PORT || 3001; // ✅ fallback
+const port = process.env.PORT; 
 
-app.use(cors());                       // ✅ now defined
+app.use(cors());                       
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+//dummy weather endpoint
+ res.json(
+    {
+        city: "Ghent",
+        country: "Belgium",
+        lat: 51.0543,
+        lon: 3.7174,
+        temperature: 15,
+        description: "Partly cloudy" 
+    }
+ )
 });
 
 app.listen(port, () => {
